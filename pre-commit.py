92eba499c9b33416c70f8c88809d72614e8a412a
Spencer Lyon <spencerlyon2@gmail.com>
2016-10-22 13:43:29 -0400

    Commit hook should be wokring now!

--- pre-commit.py
+++ pre-commit.py
@@ -3,20 +3,31 @@
 import os
 import json
 import fnmatch
+import subprocess
 
 
 def strip_output_cell(fn):
+    changed = False
     with open(fn, "r") as f:
         js = json.load(f)
         for cell in js["cells"]:
             if cell["cell_type"] == "code":
-                cell["outputs"] = []
-                cell["execution_count"] = None
+                if len(cell["outputs"]) != 0:
+                    cell["outputs"] = []
+                    changed = True
 
-    with open(fn, "w") as f:
-        json.dump(js, f)
+                if cell["execution_count"] is not None:
+                    cell["execution_count"] = None
+                    changed = True
 
-for dirpath, dirnames, files in os.walk("../../"):
+    if changed:
+        with open(fn, "w") as f:
+            json.dump(js, f)
+
+repo_root = os.popen("git rev-parse --show-toplevel").read().strip()
+for dirpath, dirnames, files in os.walk(repo_root):
     if not "/." in dirpath:
         for f in fnmatch.filter(files, "*.ipynb"):
-            strip_output_cell(os.path.join(dirpath, f))
+            fn = os.path.join(dirpath, f)
+            strip_output_cell(fn)
+            subprocess.call(["git", "add", fn])
